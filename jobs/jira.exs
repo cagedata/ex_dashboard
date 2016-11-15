e755d5161835b5bf064eb8e764d8ff30b85438bf
Dave Long <dlong@cagedata.com>
2016-11-15 11:46:04 -0500

    Move JIRA filter config from lib to job

--- jobs/jira.exs
+++ jobs/jira.exs
@@ -1,14 +1,19 @@
 use Kitto.Job.DSL
 
+filters = [
+  "service_desk_issues": "10300",
+  "unassigned_issues": "10100"
+]
+
 job :open_jira_issues, every: {2, :minutes} do
-  count = ExDashboard.APIs.Jira.incidents(:service_desk_issues)
+  count = ExDashboard.APIs.Jira.incidents(filters[:service_desk_issues])
   |> ExDashboard.APIs.Jira.count
 
   broadcast! :service_desk_issue_count, %{value: count}
 end
 
 job :unassigned_jira_issues, every: {2, :minutes} do
-  issues = ExDashboard.APIs.Jira.incidents(:unassigned_issues)["issues"]
+  issues = ExDashboard.APIs.Jira.incidents(filters[:unassigned_issues])["issues"]
     |> Enum.map(fn(incident) ->
       %{ label: incident["fields"]["summary"], value: incident["key"] }
     end)
